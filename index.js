4c707ba631f3fa7b89f03cefddc28a5ac4cbfabe
Nabil Enayet <nabil.enayet@nuvolo.com>
2020-03-09 13:41:31 -0400

    add click to change color functionality

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var colorMap = {
-    0: "#8c7ae6",
+    0: "#c23616",
     1: "#44bd32",
-    2: "#40739e",
-    3: "#c23616",
-    4: "#0097e6"
+    2: "#0097e6",
+    3: "#fa983a",
+    4: "#8c7ae6"
 };
+
+function rotateColor(current_color) {
+    for (var c in colorMap) {
+        if (colorMap[c]===current_color) {
+            return colorMap[genNextIndex(c)] || "#fff";
+        }
+    }
+    return colorMap[0];
+
+    function genNextIndex(idx) {
+        idx++;
+        if (idx>4) return -1;
+        return idx;
+    }
+}
 
 var idMap = {
     0: "zero",
@@ -50,19 +65,25 @@ function renderCard(feature) {
 
         var value = 0;
         feature.split(",").forEach(function(featureEl, idx) {
-
             value += parseInt(featureEl)>=0 ? 1 : 0;
             var thisColor = colorMap[featureEl] || "#ffffff";
             var thisId = "#" + idMap[idx];
             d3.select("#" + idVal).select(thisId)
                 .style('stroke', thisColor)
                 .style('fill', thisColor)
-                .style('fill-opacity', 0.25);
+                .style('fill-opacity', 0.25)
+                .attr('col', thisColor);
+            d3.select("#" + idVal).select(thisId).on("click", function() {
+                var lastColor = d3.select(this).attr('col');
+                var nextColor = rotateColor(lastColor);
+                d3.select(this).style('fill', nextColor)
+                               .style('stroke', nextColor)
+                               .attr('col', nextColor);
+            })
         });
 
-
         d3.select("#" + idVal).select("#val").select("tspan").text(value);
-        d3.select("#" + idVal).select("#industry").style("fill", colorMap[Math.floor(Math.random()*5)]);
+        d3.select("#" + idVal).select("#industry").style("fill", colorMap[feature.split(",")[0]]);
 
     });
 }
