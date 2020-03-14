26be882dca44ecd6d57597d605fc30bea6c9dcf3
Nabil Enayet <nabil.enayet@nuvolo.com>
2020-03-14 15:48:46 -0400

    Setup vuejs

---
+++ ff-editor/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h3> Feature Factory </h3>
+    <img src="../../assets/logo.svg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
