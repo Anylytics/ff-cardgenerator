9514950b9899c492445f65b4af7c2e25482b24ad
Nabil Enayet <nabil.enayet@nuvolo.com>
2020-03-14 16:04:33 -0400

    Add repeat industries

--- ff-editor/src/components/NavBar.vue
+++ ff-editor/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="industries">
-    <h1 v-for"industry in industries">{{ industries }}</h1>
+    <h1 v-for="industry in industries">{{ industry.name }}</h1>
   </div>
 </template>
 <script>
